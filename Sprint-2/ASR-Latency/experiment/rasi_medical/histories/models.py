ca60513b25a8e45bdf23ee4c9c7078516c42594c
fai-aher <f.hernandezt@uniandes.edu.co>
2023-09-30 11:48:00 -0500

    Addition: Modified Diagrams folder

--- Sprint-2/ASR-Latency/experiment/rasi_medical/histories/models.py
+++ Sprint-2/ASR-Latency/experiment/rasi_medical/histories/models.py
@@ -6,7 +6,6 @@ class History(models.Model):
     patient_id = models.IntegerField()
     state = models.CharField(max_length=255)
     observations = models.CharField(max_length=255)
-    # Add any other fields you need for your patient model
 
     def __str__(self):
         return self.state
