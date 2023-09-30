6be6d20715caa366d094d3d41687c20e21e814eb
fai-aher <f.hernandezt@uniandes.edu.co>
2023-09-30 01:04:33 -0500

    DATABASE settings updated in rasi-medical app

--- Sprint-2/ASR-Latency/experiment/rasi_medical/rasi_medical/settings.py
+++ Sprint-2/ASR-Latency/experiment/rasi_medical/rasi_medical/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'rasi_medical.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rasimedical-db',
+        'USER': 'rasimedical-user',
+        'PASSWORD': 'rasimedical',
+        'HOST': '10.128.0.2',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
