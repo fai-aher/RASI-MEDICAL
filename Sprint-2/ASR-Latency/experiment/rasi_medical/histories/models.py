021f4cf1fb6b83406f7555e2e64305b93f176d06
fai-aher <f.hernandezt@uniandes.edu.co>
2023-10-21 08:53:01 -0500

    Missing atts in Patient history

--- Sprint-2/ASR-Latency/experiment/rasi_medical/histories/models.py
+++ Sprint-2/ASR-Latency/experiment/rasi_medical/histories/models.py
@@ -6,6 +6,9 @@ class History(models.Model):
     patient_id = models.IntegerField()
     state = models.CharField(max_length=255)
     observations = models.CharField(max_length=255)
+    
+    # Modificar para que se guarde la historia del paciente, para que no se remplaze.
+    # Añadir más atributos para que la historia clínica sea más completa. Por ejemplo, exámenes asignados para hacerse.
 
     def __str__(self):
         return self.state
