9783bc82afa8f17e341b4b8991ddec64c223b8e7
fai-aher <f.hernandezt@uniandes.edu.co>
2023-11-11 08:54:59 -0500

    Code for Sprint 3

---
+++ Sprint-3/ASR-Availability/experiment/rasi_medical/histories/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+class History(models.Model):
+    patient_id = models.IntegerField()
+    state = models.CharField(max_length=255)
+    observations = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    blood_type = models.CharField(max_length=3)
+    allergies = models.CharField(max_length=255)
+    medications = models.CharField(max_length=255)
+    past_diseases = models.CharField(max_length=255)
+    surgeries = models.CharField(max_length=255)
+    family_history = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.state
+    
+class HistoryEdit(models.Model):
+    history = models.ForeignKey(History, on_delete=models.CASCADE)
+    edited_at = models.DateTimeField(auto_now_add=True)
+    # Copia de cada campo en History
+    patient_id = models.IntegerField()
+    state = models.CharField(max_length=255)
+    observations = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    blood_type = models.CharField(max_length=3)
+    allergies = models.CharField(max_length=255)
+    medications = models.CharField(max_length=255)
+    past_diseases = models.CharField(max_length=255)
+    surgeries = models.CharField(max_length=255)
+    family_history = models.CharField(max_length=255)
+
+class Prescription(models.Model):
+    history = models.ForeignKey(History, on_delete=models.CASCADE)
+    medication = models.CharField(max_length=255)
+    dosage = models.CharField(max_length=255)
+
+class LabResult(models.Model):
+    history = models.ForeignKey(History, on_delete=models.CASCADE)
+    test_name = models.CharField(max_length=255)
+    result = models.CharField(max_length=255)
--- Sprint-3/ASR-Availability/experiment/rasi_medical/histories/views.py
+++ Sprint-3/ASR-Availability/experiment/rasi_medical/histories/views.py
@@ -20,21 +20,34 @@ def patientHistories_view(request, patient_pk):
 
 def history_edit(request, history_pk):
     history = get_history_by_id(history_pk)
-    patient_id = history.patient_id
     if request.method == 'POST':
-        form = HistoryForm(request.POST)
+        form = HistoryForm(request.POST, instance=history)
         if form.is_valid():
-            update_history(form, history_pk, patient_id)
+            form.save()
+            # Creacion de una nueva instancia de HistoryEdit
+            HistoryEdit.objects.create(
+                history=history,
+                patient_id=history.patient_id,
+                state=history.state,
+                observations=history.observations,
+                date_of_birth=history.date_of_birth,
+                blood_type=history.blood_type,
+                allergies=history.allergies,
+                medications=history.medications,
+                past_diseases=history.past_diseases,
+                surgeries=history.surgeries,
+                family_history=history.family_history,
+            )
             messages.add_message(request, messages.SUCCESS, 'Successfully updated history')
             return HttpResponseRedirect(reverse('edit_history', args=[history_pk]))
         else:
             print(form.errors)
     else:
-        form = HistoryForm()
+        form = HistoryForm(instance=history)
 
     context = {
         'form': form,
-        'patient_id': patient_id,
+        'patient_id': history.patient_id,
     }
     return render(request, 'Histories/historyUpdate.html', context)
 
