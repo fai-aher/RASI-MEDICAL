03081c0406c67318d95db29a04fd9cce936d9007
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-12-02 20:36:37 -0500

    auth

---
+++ auth-app/main.py
@@ -0,0 +1,109 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy import create_engine, Column, Integer, String, Sequence, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from typing import List
+
+# Define the FastAPI app
+app = FastAPI()
+
+# Define SQLAlchemy models
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    username = Column(String(50), unique=True, index=True)
+    hashed_password = Column(String(100))
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+# Database configuration
+DATABASE_URL = "postgresql://auth_user:auth_password@34.132.100.17/auth_db"
+engine = create_engine(DATABASE_URL)
+
+# Create tables
+Base.metadata.create_all(bind=engine)
+
+# Dependency to get the database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Dependency to get the current user
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Invalid credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return authenticate_user(db, token, credentials_exception)
+
+# OAuth2PasswordBearer for authentication
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# OAuth2PasswordRequestForm for token retrieval
+oauth2_request_form = OAuth2PasswordRequestForm
+
+# CryptContext for password hashing
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Function to verify a user's credentials
+def verify_token(db: Session, token: str):
+    user = db.query(User).filter(User.username == token).first()
+    if user:
+        return user
+
+# Function to authenticate a user
+def authenticate_user(db: Session, token: str, credentials_exception):
+    user = verify_token(db, token)
+    if not user:
+        raise credentials_exception
+    return user
+
+# Function to create a new user
+def create_user(db: Session, username: str, password: str):
+    hashed_password = pwd_context.hash(password)
+    db_user = User(username=username, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+# Endpoint to create a new user
+@app.post("/users/", response_model=User)
+def create_user_endpoint(username: str, password: str, db: Session = Depends(get_db)):
+    return create_user(db, username, password)
+
+# Function to create a JWT token
+def create_jwt_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, "SECRET_KEY", algorithm="HS256")
+    return encoded_jwt
+
+# Token expiration time
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# Endpoint to retrieve a token
+@app.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    db_user = authenticate_user(form_data.username, form_data.password)
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_jwt_token(data={"sub": form_data.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
