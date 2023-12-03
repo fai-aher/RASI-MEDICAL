0f2448bdd379d2e65cdaf5183ba3c6b5bfb40314
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-12-02 22:25:05 -0500

    rename

--- sprint-3-version/rasi_medical/histories/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-from . import views
-
-urlpatterns = [
-    path('<int:history_pk>', views.history_edit, name='edit_history'),
-]
