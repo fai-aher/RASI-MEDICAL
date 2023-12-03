0971824f0fc2b1fced8f5399bb3b05ac1eeb16a5
Sebastian Urrea <68788933+jsurrea@users.noreply.github.com>
2023-12-03 03:48:16 -0500

    update

--- histories-app/rasi_medical/views.py
+++ histories-app/rasi_medical/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    context = {
+        'is_authenticated': True,
+        'username': "jsurrea",
+    }
     return render(request, 'index.html', context) #TODO add context is_authenticated, username
 
 def healthCheck(request):
